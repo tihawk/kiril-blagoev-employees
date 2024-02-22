9cca0f76a77577df314380473ea96dbedecdae66
tihawk <cccp.hawk@gmail.com>
2024-02-22 11:24:40 +0200

    initial commit 💥

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Button, Typography } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import styled from '@emotion/styled'
+import { CloudUpload } from '@mui/icons-material'
+
+const StyledButton = styled(Button)`
+  color: #61dafb;
+`
+
+const VisuallyHiddenInput = styled('input')`
+  clip: rect(0 0 0 0);
+  clippath: inset(50%);
+  height: 1;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  whitespace: nowrap;
+  width: 1;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Typography>
+          Upload a <code>CSV</code> file to display data.
+        </Typography>
+        <Button
+          component="label"
+          variant="contained"
+          startIcon={<CloudUpload />}
         >
-          Learn React
-        </a>
+          Upload File
+          <VisuallyHiddenInput type="file" />
+        </Button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
